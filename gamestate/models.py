ad91f12915131898c09ccb54ac7543bfd81665e4
Manu Jhawar <153882497+manwhar@users.noreply.github.com>
2026-07-03 22:36:05 -0700

    Added __str__ method for Vec3d

--- gamestate/models.py
+++ gamestate/models.py
@@ -58,6 +58,9 @@ class Vec3d:
     def __hash__(self) -> int:
         return hash((self.x, self.y, self.z))
 
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y}, {self.z})"
+
     def copy(self) -> Vec3d:
         return Vec3d(self.x, self.y, self.z)
 
